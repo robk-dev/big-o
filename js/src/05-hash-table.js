6acc64581f5d8fd1c9577c782c89cad3b7b14763
Robert Kardjaliev <robk-dev@users.noreply.github.com>
2020-10-14 15:33:03 +0100

    feat: added utility methods to hashTable

--- js/src/05-hash-table.js
+++ js/src/05-hash-table.js
@@ -5,34 +5,84 @@ const hashString = (s, table_size) => {
 
     for (const char of str) {
         const charCode = char.charCodeAt(0);
-        hash *= charCode % table_size;
+        hash *= 17 * charCode % table_size;
     }
     return hash;
 }
 
-const HashTable = (num_buckets = 10000, _resize_percent = 0.8) => {
-    let items = new Array(num_buckets);
+const HashTable = (num_buckets = 10000, resize_percent = 0.8) => {
+    let buckets = new Array(num_buckets);
+    let item_count = 0;
 
     return {
+        count: () => item_count,
+        size: () => buckets.length,
+        getTable: () => buckets,
         get: (key) => {
-            const hash = hashString(key, num_buckets);
-            const bucket = items[hash];
+            const hash = hashString(key, buckets.length);
+            const bucket = buckets[hash];
             if (!bucket) {
                 return null;
             }
-            return bucket.find(([k, _v]) => k === key)[1];
+            const res = bucket.find(([k, _v]) => k === key); // [1] for value
+            return res ? res[1] : res;
         },
-        set: (key, value) => {
-            const hash = hashString(key, num_buckets);
-            const bucket = items[hash];
+        set: function (key, value) {
+
+            if (item_count / buckets.length > resize_percent) {
+                this.resize();
+            }
+
+            const hash = parseInt(hashString(key, buckets.length));
+            const bucket = buckets[hash];
+
+            if (!bucket || !bucket.length) {
+                buckets[hash] = [[key, value]];
+            } else {
+                buckets[hash].push([key, value]);
+            }
+            item_count++;
+        },
+        delete: (key) => {
+            const bucket = buckets[hashString(key, buckets.length)];
 
             if (!bucket) {
-                items[hash] = [[key, value]];
-            } else {
-                items[hash].push([key, value]);
+                return false;
             }
+            const idx = bucket.findIndex(([k, _v]) => k === key);
+            const res = bucket.splice(idx, 1);
+
+            if (res && res.length > 0) {
+                item_count--;
+                return true;
+            }
+            return false;
+        },
+        resize: () => {
+            const new_table = new Array(buckets.length * 2);
+            temp = buckets;
+
+            buckets.forEach(b => {
+                console.log({ msg: 'resize()', b });
+                if (b && b.length > 0) {
+                    b.forEach(([k, v]) => {
+                        const hash = parseInt(hashString(k, new_table.length));
+
+                        console.log({ hash, type: typeof hash });
+                        const bucket = new_table[hash];
+
+                        if (!bucket || !bucket.length) {
+                            new_table[hash] = [[k, v]];
+                        } else {
+                            new_table[hash].push([k, v]);
+                        }
+                    })
+                }
+            });
+
+            buckets = new_table;
         }
-    };
+    }
 };
 
 module.exports = HashTable;
