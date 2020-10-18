c5acd250b9b8c998332b5a109a94967b4f703d2b
Robert Kardjaliev <robk-dev@users.noreply.github.com>
2020-10-18 22:07:14 +0100

    feat(sorting):started bubble sort

--- js/src/sorting-algos/index.js
+++ js/src/sorting-algos/index.js
@@ -1,8 +1,10 @@
 const InsertionSort = require('./01-insertion-sort');
 const MergeSort = require('./02-merge-sort');
 const QuickSort = require('./03-quick-sort');
+const BubbleSort = require('./04-bubble-sort');
 
 module.exports = {
+    BubbleSort,
     InsertionSort,
     MergeSort,
     QuickSort
--- js/test/sorting-algos/00-all.spec.js
+++ js/test/sorting-algos/00-all.spec.js
@@ -1,5 +1,6 @@
 const algos = require('../../src/sorting-algos/index');
 const random_order = [1, 5, 6, 29, 2, 3, 4, 18, 19, 0, 20, 10, 12, 16, 17, 28, 11, 30, 7, 8, 9, 22, 23, 24, 13, 14, 15, 25, 26, 27, 21];
+const random_order2 = [99, 98, 95, 13, 14, 15, 16, 7, 9, 2, 8, 999, 0, -12];
 const in_order = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 
 describe("Sorting Algorithms Suite:", () => {
@@ -21,7 +22,6 @@ describe("Sorting Algorithms Suite:", () => {
     });
 
     test("merge_sort(array) should work if array.length > 1, else should return array", () => {
-        const random_order2 = [99, 98, 95, 13, 14, 15, 16, 7, 9, 2, 8, 999, 0, -12];
         const expected = [-12, 0, 2, 7, 8, 9, 13, 14, 15, 16, 95, 98, 99, 999];
 
         const ordered = algos.MergeSort(random_order2);
@@ -39,5 +39,14 @@ describe("Sorting Algorithms Suite:", () => {
 
     test("quick_sort(array) should return [] when called with no arguments", () => {
         expect(algos.QuickSort()).toEqual([]);
+    });
+
+    test("bubble_sort(array) should work if array.length > 1, else should return array", () => {
+        const ordered = algos.BubbleSort(random_order);
+        expect(ordered).toEqual(in_order);
+    });
+
+    test("bubble_sort(array) should return [] when called with no arguments", () => {
+        expect(algos.BubbleSort()).toEqual([]);
     });
 });
